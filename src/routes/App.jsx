f1d1ab43b18603d2ed67a9fb28a022dbdeed1b36
cyrus01337 <cyrus21337@gmail.com>
2022-02-11 11:24:45 +0000

    Setup to support base React project

---
+++ src/routes/App.jsx
@@ -0,0 +1,16 @@
+// import PropTypes from "prop-types";
+import React from "react";
+
+
+function App() {
+    return (
+        <div id="app">
+            <h1>index</h1>
+
+            <p>index</p>
+        </div>
+    );
+}
+
+
+export default App;
